d94d01991ac8d3b5b6c5dde246658c36d85c95a6
Francis Carino <fgcarino@gmail.com>
2022-09-21 11:23:37 -0700

    complete tree

--- tree.js
+++ tree.js
@@ -15,20 +15,63 @@ class Tree {
   /** sumValues(): add up all of the values in the tree. */
 
   sumValues() {
-    
+
+    if (this.root == null) return 0;
+
+    let sum = 0;
+    let stack = [this.root];
+
+    while (stack.length) {
+      let current = stack.pop();
+      sum += current.val;
+
+      for (let child of current.children) {
+        stack.push(child);
+      }
+    }
+    return sum;
   }
 
   /** countEvens(): count all of the nodes in the tree with even values. */
 
   countEvens() {
 
+    if (this.root == null) return 0;
+
+    let count = 0;
+    let stack = [this.root];
+
+    while (stack.length) {
+      let current = stack.pop();
+
+      if (current.val % 2 === 0) count++;
+
+      for (let child of current.children) {
+        stack.push(child);
+      }
+    }
+    return count;
   }
 
   /** numGreater(lowerBound): return a count of the number of nodes
    * whose value is greater than lowerBound. */
 
   numGreater(lowerBound) {
+    if (this.root == null) return 0;
 
+    let count = 0;
+    let stack = [this.root];
+
+    while (stack.length) {
+      let current = stack.pop();
+
+      if (current.val > lowerBound) count++;
+
+      for (let child of current.children) {
+        stack.push(child);
+      }
+    }
+    return count;
   }
 }
 
